cbf778ecd1482a0b398ffdb3067b56383150b050
kole <misterija@gmail.com>
2024-12-24 14:32:34 +0100

    add middleware with alias permissions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\PermissionMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->append(PermissionMiddleware::class);
+        $middleware->alias([
+            'permissions' => PermissionMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
