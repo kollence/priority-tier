16560c23a3a0c649b252537b5683f713c5ed7af9
kole <misterija@gmail.com>
2024-12-24 14:37:05 +0100

    Simple Dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,51 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+@extends('layouts.admin')
+@section('title', 'User')
+@section('content')
+<h1 class="mt-4">{{ __('Dashboard (profile/index)') }}</h1>
+<div class="card mb-4">
+    <div class="card-body">
+        @if(session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
+            {{ session('success') }}
+        </div>
+        @endif
+    </div>
+</div>
+<div class="card mb-4">
+    <div class="card-body">
+        <table id="datatables">
+            <thead>
+                <tr>
+                    <th>Name</th><td>{{$user->name}}</td>
+                </tr>
+                <tr>
+                    <th>Email</th><td>{{$user->email}}</td>
+                <tr>
+                    <th>Permissions</th><td>
+                        @foreach($user->permissions as $permission)
+                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800 mr-2">
+                            {{ $permission->name }}
+                        </span>
+                        @endforeach
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                    
+                    
+                </tr>
+            </tbody>
+        </table>
+        <div class="row justify-content-end">
+            <div class="col-md-6 d-flex justify-content-end">
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+<script>
+</script>
+
+
+@endsection
