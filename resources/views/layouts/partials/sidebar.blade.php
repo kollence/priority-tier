281ffb031606276239c25d6eeff4b5da9a5250c8
kole <misterija@gmail.com>
2024-12-24 14:41:22 +0100

    sidebar with Gate for restricted routes

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,10 @@
+@php
+    $importTypes = config('import_types');
+@endphp
 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
     <div class="sb-sidenav-menu">
         <div class="nav">
+        @can('restrict-users-route')
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#usersManagement" aria-expanded="false" aria-controls="usersManagement">
                 <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                 User Management
@@ -12,7 +16,8 @@
                     <a class="nav-link" href="{{route('permissions.index')}}">Permissions</a>
                 </nav>
             </div>
-            <a class="nav-link" href="charts.html">
+            @endcan
+            <a class="nav-link" href="{{route('data-import.index')}}">
                 <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                 Data Import
             </a>
@@ -23,34 +28,21 @@
             </a>
             <div class="collapse" id="importedData" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                 <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                        Authentication
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="login.html">Login</a>
-                            <a class="nav-link" href="register.html">Register</a>
-                            <a class="nav-link" href="password.html">Forgot Password</a>
-                        </nav>
-                    </div>
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                        Error
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="401.html">401 Page</a>
-                            <a class="nav-link" href="404.html">404 Page</a>
-                            <a class="nav-link" href="500.html">500 Page</a>
-                        </nav>
-                    </div>
+                @foreach($importTypes as $key => $type)
+                    @can($type['permission_required'])
+                        <!-- <option value="{{ $key }}">{{ $type['label'] }}</option> -->
+                        @foreach($type['files'] as $file)
+                            <a class="nav-link" href="#">{{$type['label']}} {{$file['label']}}</a>
+                        @endforeach
+                    @endcan
+                @endforeach
                 </nav>
             </div>
             <a class="nav-link" href="tables.html">
                 <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                 Imports
             </a>
+            
         </div>
     </div>
 </nav>
