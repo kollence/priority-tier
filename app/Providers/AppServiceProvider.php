0052e71ff249456d144ca52f3575749280f690e6
kole <misterija@gmail.com>
2024-12-24 14:30:57 +0100

    Gates for grouped permissions and for exact ones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        Gate::define('manage-users', function ($user) {
+            return $user->hasPermission(['user-admin', 'user-management']);
+        });
+    
+        Gate::define('access-data', function ($user) {
+            return $user->hasPermission(['import-orders', 'import-products', 'import-customers']);
+        });
+    
+        Gate::define('restrict-users-route', function ($user) {
+            return !$user->hasPermission(['import-orders', 'import-products', 'import-customers']) && !$user->permissions || empty($user->permissions);
+        });
+
+        Gate::define('import-orders', function ($user) {
+            return $user->hasPermission('import-orders');
+        });
+        Gate::define('import-products', function ($user) {
+            return $user->hasPermission('import-products');
+        });
+        Gate::define('import-customers', function ($user) {
+            return $user->hasPermission('import-customers');
+        });
     }
 }
