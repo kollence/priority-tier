caff4ec4493206d562a3d8172c6862773e7275ab
kole <misterija@gmail.com>
2024-12-26 01:04:48 +0100

    Import error Mail markup

---
+++ resources/views/emails/import_error.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <ul>
+        <li><strong>Data Import ID:</strong> {{ $importLog['data_import_id'] }}</li>
+        <li><strong>Row Number:</strong> {{ $importLog['row_number'] }}</li>
+        <li><strong>Column Name:</strong> {{ $importLog['column_name'] }}</li>
+        <li><strong>Invalid Value:</strong> {{ $importLog['invalid_value'] }}</li>
+        <li><strong>Validation Message:</strong> {{ $importLog['validation_message'] }}</li>
+    </ul>
+</div>
