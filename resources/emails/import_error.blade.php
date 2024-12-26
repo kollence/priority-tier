d2fb4ec063def2174129782228b7d3c20e9ead49
kole <misterija@gmail.com>
2024-12-26 01:05:21 +0100

    removed from wrong place it was

--- resources/emails/import_error.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <ul>
-        <li><strong>Data Import ID:</strong> {{ $importLog->data_import_id }}</li>
-        <li><strong>Row Number:</strong> {{ $importLog->row_number }}</li>
-        <li><strong>Column Name:</strong> {{ $importLog->column_name }}</li>
-        <li><strong>Invalid Value:</strong> {{ $importLog->invalid_value }}</li>
-        <li><strong>Validation Message:</strong> {{ $importLog->validation_message }}</li>
-    </ul>
-</div>
