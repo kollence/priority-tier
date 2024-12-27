36b30fd427d7876255163afa96d465d30f2a5088
kole <misterija@gmail.com>
2024-12-27 03:11:43 +0100

    comment out not functional packages

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,40 +3,40 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // resources/js/bootstrap.js
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    cluster: import.meta.env.VITE_REVERB_APP_CLUSTER,
-    // key: import.meta.env.VITE_REVERB_APP_KEY,
-    // wsHost: import.meta.env.VITE_REVERB_HOST,
-    // wsPort: import.meta.env.VITE_REVERB_PORT,
-    // wssPort: import.meta.env.VITE_REVERB_PORT,
-    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    // enabledTransports: ['ws', 'wss'],
-});
+// window.Pusher = Pusher;
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     cluster: import.meta.env.VITE_REVERB_APP_CLUSTER,
+//     // key: import.meta.env.VITE_REVERB_APP_KEY,
+//     // wsHost: import.meta.env.VITE_REVERB_HOST,
+//     // wsPort: import.meta.env.VITE_REVERB_PORT,
+//     // wssPort: import.meta.env.VITE_REVERB_PORT,
+//     // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     // enabledTransports: ['ws', 'wss'],
+// });
 
-// Vue/React component example
-const userId = document.querySelector('meta[name="user-id"]').content;
+// // Vue/React component example
+// const userId = document.querySelector('meta[name="user-id"]').content;
 
-Echo.channel(`import-notifications.${userId}`)
-    .listen('DataImportCompleted', (e) => {
-        if (e.status === 'success') {
-            showSuccessNotification(e.message);
-        } else {
-            showErrorNotification(e.message);
-        }
-    });
+// Echo.channel(`import-notifications.${userId}`)
+//     .listen('DataImportCompleted', (e) => {
+//         if (e.status === 'success') {
+//             showSuccessNotification(e.message);
+//         } else {
+//             showErrorNotification(e.message);
+//         }
+//     });
 
-function showSuccessNotification(message) {
-    // Implement your popup/toast notification
-    alert(message);
-}
+// function showSuccessNotification(message) {
+//     // Implement your popup/toast notification
+//     alert(message);
+// }
 
-function showErrorNotification(message) {
-    // Implement your popup/toast notification
-    alert(message);
-}
+// function showErrorNotification(message) {
+//     // Implement your popup/toast notification
+//     alert(message);
+// }
